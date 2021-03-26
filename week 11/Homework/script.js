97dfd74e6551d95415ca9cc590a2c3ff6d132eb0
BuildTools <unconfigured@null.spigotmc.org>
2021-03-26 18:03:46 -0400

    Homework week 11

--- week 11/Homework/script.js
+++ week 11/Homework/script.js
@@ -1,13 +1,47 @@
-var ToDoItem = document.getElementById("input").textContent;
-var Button = document.getElementById("Button");
 
+document.getElementById("AddButton").onclick = function()
+{
+    var node = document.createElement("Li");
+   node.setAttribute("id","li")
+    var text = document.getElementById("input").value; 
+    var textnode=document.createTextNode(text);
 
+    //checks for input data before appending
+    if(text !="")
+    {
+        node.appendChild(textnode);
+    document.getElementById("TDList").appendChild(node);
+    }
+    //Alerts user to input data before pressing buton
+    else
+    {
+        window.alert("Please put an item in the text box before clicking the button.");   
+    }
 
-function AddItem(){
-var newLI = document.createElement('li');
-var newItem = document.createTextNode(ToDoItem);
-var positionLI = document.getElementsByTagName('ul')[0];
-// Insert the new element into its position.
-newLI.appendChild(newItem);
-positionLI.appendChild(newItem);
+    DeleteInput();
 }
+ 
+//deletes value in input text box
+function DeleteInput()
+{
+    document.getElementById("input").value="";
+    document.getElementById("input").focus();
+}
+
+//Clears all data in UL
+document.getElementById("ClearButton").onclick = function()
+{
+    var ul = document.getElementById("TDList");
+    while(ul.firstChild) ul.removeChild(ul.firstChild);
+}
+
+//checks for what type of element is clicked and changes textDecoration if it is a list item
+document.addEventListener ('click', function(event)
+{
+   if (event.target.id==="li"){
+        event.target.id = 'checked';
+        var CrossThrough = document.getElementById('checked');
+        CrossThrough.style.textDecoration= 'line-through'
+   event.target.id = 'li';
+    }
+},false)
